082a368fc46c20bb0fe7d0b51d40df007457f231
Super Zombi <super.zombi.yt@gmail.com>
2026-07-21 16:31:37 +0100

    print asset.url

--- examples/assets.py
+++ examples/assets.py
@@ -2,6 +2,9 @@ import discordrpc
 
 rpc = discordrpc.RPC(app_id=123456789)
 
+for asset in rpc.assets:
+      print(asset.url)
+
 rpc.set_activity(
       state="Assets example",
       large_image=rpc.assets.get("cat")
